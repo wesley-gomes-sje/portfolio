64f5b0215e605f5102c5abe294403d0727b930f4
wesley-gomes-sje <57814784+wesley-gomes-sje@users.noreply.github.com>
2021-10-31 18:53:42 -0300

    feito modulo de qualificação

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,3 +45,26 @@ function toggleSkills(){
 skillsHeader.forEach((el) =>{
     el.addEventListener('click', toggleSkills)
 })
+
+
+/*Mostrar qualificações*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click',() =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+
+    })
+})
