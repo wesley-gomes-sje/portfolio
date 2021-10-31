c118ae4f6077d430c6262ad5fc8378e11d3c4664
wesley-gomes-sje <57814784+wesley-gomes-sje@users.noreply.github.com>
2021-10-31 20:18:07 -0300

    Parte de serviços criado

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,7 +64,28 @@ tabs.forEach(tab =>{
             tab.classList.remove('qualification__active')
         })
         target.classList.add('qualification__active')
+    })
+})
 
+/*Mostrar serviços*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close')
 
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) =>{
+    modalBtn.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) =>{
+    modalClose.addEventListener('click', () =>{
+        modalViews.forEach((modalView) =>{
+            modalView.classList.remove('active-modal')
+        })
     })
 })
