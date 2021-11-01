ff3c87bbd3a3b5814f3bb958087b128987d6b1c3
wesley-gomes-sje <57814784+wesley-gomes-sje@users.noreply.github.com>
2021-10-31 23:00:33 -0300

    pagina portifolio criada

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,3 +89,18 @@ modalCloses.forEach((modalClose) =>{
         })
     })
 })
+
+/*Slide portfolio*/
+let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+    },
+  });
+
